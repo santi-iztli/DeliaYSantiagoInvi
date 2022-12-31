ca795d0ab1bc3dda3ce0e0e1d2e00326567a9ceb
santi-iztli <santi.iztli@gmail.com>
2022-12-31 13:20:45 -0600

    [Social media moved to contact]

--- js/alerta.js
+++ js/alerta.js
@@ -1,24 +1,24 @@
 
 function alerta() {
-    Swal.fire({
-        imageUrl: './img/invitacion.gif',
-        imageWidth: 150,
-        imageHeight: 150,
-        text:'Delia y Santiago 🎉',
-        title:'Has sido invitado a la boda de:',
-        confirmButtonText: 'Abrir',
-        color: '#000',
-        background: '#ffffffb3',
-        backdrop: 'rgba(0,0,0,0.7)',
-        showClass: {
-            popup: 'animate__animated animate__backInDown'
-          },
-          hideClass: {
-            popup: 'animate__animated animate__bounceOut'
-          },
-          footer:'Invitacion para 2 personas'
+  Swal.fire({
+    imageUrl: './img/invitacion.gif',
+    imageWidth: 150,
+    imageHeight: 150,
+    text: 'Delia y Santiago 🎉',
+    title: 'Has sido invitado a la boda de:',
+    confirmButtonText: 'Abrir',
+    color: '#000',
+    background: '#ffffffb3',
+    backdrop: 'rgba(0,0,0,0.7)',
+    showClass: {
+      popup: 'animate__animated animate__backInDown'
+    },
+    hideClass: {
+      popup: 'animate__animated animate__bounceOut'
+    },
+    footer: 'Invitacion para 2 personas'
 
 
-    });
+  });
 }
 alerta();
